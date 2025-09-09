d3e610026a64ac0250fcafcf7845b36a089600d3
farhanch <farhanch0443@gmail.com>
2025-09-09 23:43:50 +0600

    trees, show done

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 => if we use let we cant change its value later. but with const we can not change it except its an object or array. var is almost same like let but with variable issue.
 
 2) What is the difference between map(), forEach(), and filter()? 
-=> 
+=> map creates a new array with changed values. foreach()  runs a function for each item. filter() creates a new array
 
 3) What are arrow functions in ES6?
 => 
--- index.js
+++ index.js
@@ -45,3 +45,60 @@ fetch('https://openapi.programming-hero.com/api/categories')
             cat.innerHTML += temp;
         }
     });
+
+// plants
+fetch('https://openapi.programming-hero.com/api/plants')
+    .then(response => response.json())
+    .then(data => {
+        const tree = document.getElementById("trees");
+        tree.innerHTML = '';
+        for (let i = 0; i < data.plants.length; i++) {
+            const temp = `<div class="bg-white shadow-lg rounded-lg p-4" id="${data.plants[i].id}">
+                    <img src="${data.plants[i].image}" class="h-32 w-64 rounded-lg" alt="">
+                    <p class="py-2 plant_name cursor-pointer" onClick="setModalBox(${data.plants[i].id})">${data.plants[i].name}</p>
+                    
+                    <p class="text-xs py-2">${data.plants[i].description}</p>
+                    <div class="flex justify-between ">
+                        <button class="rounded-full bg-green-100 text-green-700 text-xs px-2 py-2">${data.plants[i].category}</button>
+                        <p>${data.plants[i].price}</p>
+                    </div>
+                    <button class="rounded-full bg-green-700 text-white w-full py-2 my-4" onClick=add_to_cart(${data.plants[i].id})>Add to Cart</button>
+                </div>`
+            tree.innerHTML += temp;
+        }
+    });
+
+// trees
+function show_category(btn) {
+    document.getElementById("trees").innerHTML = loader_spiner;
+
+    const id = btn.getAttribute("id");
+    fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            const buttons = document.querySelectorAll('#category .categories');
+
+            buttons.forEach(bton => {
+                bton.classList.remove('bg-green-700', 'text-white');
+                bton.classList.add('text-black-700');
+            });
+            btn.classList.add('bg-green-700', 'text-white');
+            btn.classList.remove('text-black-700');
+
+            const tree = document.getElementById("trees");
+            tree.innerHTML = "";
+            for (let i = 0; i < data.plants.length; i++) {
+                const temp = `<div class="bg-white shadow-lg rounded-lg p-4" id="${data.plants[i].id}">
+                    <img src="${data.plants[i].image}" class="h-32 w-64 rounded-lg" alt="">
+                    <p class="py-2 plant_name cursor-pointer" onClick="setModalBox(${data.plants[i].id})">${data.plants[i].name}</p>
+                    <p class="text-xs  py-2">${data.plants[i].description}</p>
+                    <div class="flex justify-between ">
+                        <button class="rounded-full bg-green-100 text-green-700 text-xs px-2 py-2">${data.plants[i].category}</button>
+                        <p>${data.plants[i].price}</p>
+                    </div>
+                    <button class="rounded-full bg-green-700 text-white w-full py-2 my-4" onClick=add_to_cart(${data.plants[i].id})>Add to Cart</button>
+                </div>`
+                tree.innerHTML += temp;
+            }
+        });
+}
