f1a0469c15590c579919614e32fd242b45ce33e0
farhanch <farhanch0443@gmail.com>
2025-09-09 23:45:05 +0600

    pants, modal done

--- index.js
+++ index.js
@@ -102,3 +102,75 @@ function show_category(btn) {
             }
         });
 }
+
+//show all
+function show_all(btn) {
+    document.getElementById("trees").innerHTML = loader_spiner;
+
+    fetch('https://openapi.programming-hero.com/api/plants')
+        .then(response => response.json())
+        .then(data => {
+            const buttons = document.querySelectorAll('#category .categories');
+
+            buttons.forEach(bton => {
+                bton.classList.remove('bg-green-700', 'text-white');
+                bton.classList.add('text-black-700');
+            });
+            btn.classList.add('bg-green-700', 'text-white');
+            btn.classList.remove('text-black-700');
+            const tree = document.getElementById("trees");
+            tree.innerHTML = "";
+            for (let i = 0; i < data.plants.length; i++) {
+                const temp = `<div class="bg-white shadow-lg rounded-lg p-4" id="${data.plants[i].id}">
+                    <img src="${data.plants[i].image}" class="h-32 w-64 rounded-lg" alt="">
+                    <p class="py-2 plant_name cursor-pointer" onClick="setModalBox(${data.plants[i].id})">${data.plants[i].name}</p>
+                    <p class="text-xs  py-2">${data.plants[i].description}</p>
+                    <div class="flex justify-between ">
+                        <button class="rounded-full bg-green-100 text-green-700 text-xs px-2 py-2">${data.plants[i].category}</button>
+                        <p>${data.plants[i].price}</p>
+                    </div>
+                    <button class="rounded-full bg-green-700 text-white w-full py-2 my-4" onClick=add_to_cart(${data.plants[i].id})>Add to Cart</button>
+                </div>`
+                tree.innerHTML += temp;
+            }
+        });
+
+}
+
+// modal
+function setModalBox(id) {
+    fetch(`https://openapi.programming-hero.com/api/plant/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            const plant = data.plants;
+
+            const modalWrapper = document.createElement("div");
+            modalWrapper.id = "modal";
+            modalWrapper.className = "fixed inset-0 flex items-center justify-center";
+
+            modalWrapper.innerHTML = `
+        <div class="bg-white p-6 rounded shadow-[0_25px_80px_rgba(0,0,0,0.8)] w-3/4 lg:w-1/3">
+          <img src="${plant.image}" class="h-32 w-full rounded-lg" alt="">
+          <p class="py-2 font-bold">${plant.name}</p>
+          <p class="text-xs py-2">${plant.description}</p>
+          <div class="flex justify-between">
+            <button class="rounded-full bg-green-100 text-green-700 text-xs px-2 py-2">${plant.category}</button>
+            <p>${plant.price}</p>
+          </div>
+          <button class="rounded-full bg-green-700 text-white w-full py-2 my-4" onClick=add_to_cart(${plant.id})>Add to Cart</button>
+          <div class="flex justify-center">
+            <button id="closeModal" class="bg-red-500 text-white px-2 py-1 rounded cursor-pointer">Close</button>
+          </div>
+          
+        </div>
+      `;
+            document.body.appendChild(modalWrapper);
+            document.getElementById("closeModal").addEventListener("click", () => {
+                modalWrapper.remove();
+            });
+
+            modalWrapper.addEventListener("click", (e) => {
+                if (e.target === modalWrapper) modalWrapper.remove();
+            });
+        });
+}
